e60fbc52418bcfe67383e35a1d904c8ed59cad72
Haidar Mohsen <haidarf2008@yahoo.com>
2024-10-11 15:00:56 +0100

    env and playerinput

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moveValue;
+    public float speed;
+    private int count;
+    private int numPickups = 6; //Put here the number of pickups you have.
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    void Start()
+    {
+        // Initialize the count variable to 0
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString(); if (count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
